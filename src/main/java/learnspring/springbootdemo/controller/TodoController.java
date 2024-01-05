2e9643650a93dbbf95d68c4796a0084d05dd9443
JivanSalunke <jsalunke7197@gmail.com>
2024-01-05 12:23:27 +0530

    Added delete task functionality

--- src/main/java/learnspring/springbootdemo/controller/TodoController.java
+++ src/main/java/learnspring/springbootdemo/controller/TodoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RequestMapping("/todo")
 public class TodoController {
     List<Task> tasks= new ArrayList<>();
-    private int newTaskId =0;
+    private int newTaskId = 1;
 
     @GetMapping("")
     public List<Task> getTasks(){
@@ -26,6 +26,22 @@ public class TodoController {
         task.setTaskId(newTaskId++);
         tasks.add(task);
         return task;
+    }
+
+    @DeleteMapping("/deleteTask/{id}")
+    public String deleteTask(@PathVariable("id") Integer id) throws Exception {
+        int taskIndex=-1;
+        for( int i=0;i<tasks.size();i++){
+            if(tasks.get(i).getTaskId().equals((id))){
+                taskIndex=i;
+                break;
+            }
+        }
+        if(taskIndex==-1){
+            throw new Exception("Task not found");
+        }
+        tasks.remove(taskIndex);
+        return "Task Deleted Successfully";
     }
 
 }
